97455bce14bd43a226f942a1bad8e70eec30c2b3
emr9018 <emr9018@rit.edu>
2020-02-11 16:04:20 -0500

    Update Equipment.java

--- PE03/Equipment.java
+++ PE03/Equipment.java
@@ -12,7 +12,7 @@ public class Equipment
    // Initialized variables
    int equipmentId = 0;
    int equipmentCapacity = 0;
-   int row = 0;
+   int record = 0;
 
    String equipmentName = "";
    String equipmentDescription = "";
@@ -38,5 +38,103 @@ public class Equipment
    }
    
    // Accessors and Mutators
+   public int getEquipmentId()
+   {
+      return equipmentId; 
+   }
+   
+   public void setEquipmentId(int _equipmentId)
+   {
+      equipmentId = _equipmentId;
+   }
+   
+   public int getEquipmentCapacity()
+   {
+      return equipmentCapacity;
+   }
+   
+   public void setEquipmentCapacity(int _equipmentCapacity)
+   {
+      equipmentCapacity = _equipmentCapacity;
+   }
+   
+   public String getEquipmentName()
+   {
+      return equipmentName;
+   }
+   
+   public void setEquipmentName(String _equipmentName)
+   {
+      equipmentName = _equipmentName;
+   }
+   
+   public String getEquipmentDescription()
+   {
+      return equipmentDescription;
+   }
+   
+   public void setEquipmentDescription(String _equipmentDescription)
+   {
+      equipmentDescription = _equipmentDescription;
+   }
+   
+   public int getRecord()
+   {
+      return record;
+   }
+   
+   public void fetch()
+   {
+      Connection connection = msd.connect();
+      String query = "SELECT * FROM equipment WHERE equipID = " + getEquipmentId() + ";";
+      ArrayList<ArrayList<String>> twoDimensionalArray = twoDimensionalArray = msd.getData(query, 4);;
+      
+      if (twoDimensionalArray.size() != 0)
+      {
+         setEquipmentId(Integer.parseInt(twoDimensionalArray.get(0).get(0)));
+         System.out.println(Integer.parseInt(twoDimensionalArray.get(0).get(0)));
+         setEquipmentName(twoDimensionalArray.get(0).get(1));
+         setEquipmentDescription(twoDimensionalArray.get(0).get(2));
+         setEquipmentCapacity(Integer.parseInt(twoDimensionalArray.get(0).get(3)));
+         
+         msd.close(connection);
+      }
+
+
+   } // end fetch
+   
+   public void put()
+   {
+      Connection connection = msd.connect();
+      String query = "Update Equipment SET EquipID = " + getEquipmentId() + ", EquipmentName = '" + getEquipmentName() + "', EquipmentDescription = '" + getEquipmentDescription() + "', EquipmentCapacity = " + getEquipmentCapacity() + " Where EquipID = " + getEquipmentId() + ";";
+               
+      record = msd.setData(query);
+            
+      if(record > 0)
+      {
+         this.record = record;
+      }
+      
+      msd.close(connection);
+   
+   } // end put   
+
+   public void post()
+   { 
+      Connection connection = msd.connect();
+      String query = "INSERT INTO Equipment (EquipID,EquipmentName, EquipmentDescription, EquipmentCapacity)" + " VALUES (" + getEquipmentId() + ", '" +getEquipmentName()+"', '" + getEquipmentDescription() + "', " + getEquipmentCapacity() + " );";
+          
+      record = msd.setData(query);
+      msd.close(connection);
+   } // end post
+   
+   public void delete()
+   {
+      Connection connection = msd.connect();
+      String query = "DELETE FROM Equipment WHERE EquipID = " + getEquipmentId() + ";";
+      record = msd.setData(query);
+            
+      msd.close(connection);
+   }
 
 }
