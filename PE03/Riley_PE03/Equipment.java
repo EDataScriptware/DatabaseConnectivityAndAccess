66d2b3b6dd566123dcdf0c0084a5613d2323577c
Edward Riley <55459510+emr9018@users.noreply.github.com>
2020-02-14 00:32:37 -0500

    turned in

---
+++ PE03/Riley_PE03/Equipment.java
@@ -0,0 +1,177 @@
+/* 
+   Name: Edward Riley
+   Professor: Stephen Zilora
+   Course: Database Connectivity and Access
+   Date: Feburary 14, 2020
+*/
+
+import java.sql.*;
+import javax.sql.*;
+import java.util.*;
+
+
+public class Equipment
+{
+   // Connecting Classes Together
+   static MySQLDatabase msd = new MySQLDatabase();
+   static main m = new main();
+   
+   // Initialized variables
+   int equipmentId = 0;
+   int equipmentCapacity = 0;
+   int record = 0;
+
+   String equipmentName = "";
+   String equipmentDescription = "";
+   
+   // Default constructor
+   public Equipment()
+   {
+      
+   }
+   
+   // equipmentId constructor
+   public Equipment(int _equipmentId)
+   {
+      equipmentId = _equipmentId;
+   }
+   
+   // equipment full constructor
+   public Equipment(int _equipmentId, int _equipmentCapacity, String _equipmentName, String _equipmentDescription)
+   {
+      equipmentId = _equipmentId;
+      equipmentCapacity = _equipmentCapacity;
+      equipmentName = _equipmentName;
+      equipmentDescription = _equipmentDescription;
+   }
+   
+   // Accessors and Mutators
+   public int getEquipmentId()
+   {
+      return equipmentId; 
+   }
+   
+   public void setEquipmentId(int _equipmentId)
+   {
+      equipmentId = _equipmentId;
+   }
+   
+   public int getEquipmentCapacity()
+   {
+      return equipmentCapacity;
+   }
+   
+   public void setEquipmentCapacity(int _equipmentCapacity)
+   {
+      equipmentCapacity = _equipmentCapacity;
+   }
+   
+   public String getEquipmentName()
+   {
+      return equipmentName;
+   }
+   
+   public void setEquipmentName(String _equipmentName)
+   {
+      equipmentName = _equipmentName;
+   }
+   
+   public String getEquipmentDescription()
+   {
+      return equipmentDescription;
+   }
+   
+   public void setEquipmentDescription(String _equipmentDescription)
+   {
+      equipmentDescription = _equipmentDescription;
+   }
+   
+   public int getRecord()
+   {
+      return record;
+   }
+   
+   
+   // fetch method - retrieves the data
+   public void fetch()
+   {
+      Connection connection = msd.connect();
+      msd.setConnection(connection);
+      String query = "SELECT * FROM equipment WHERE equipID = " + getEquipmentId() + ";";
+      ArrayList<ArrayList<String>> twoDimensionalArray = twoDimensionalArray = msd.getData(query, 4);
+      
+      if (twoDimensionalArray.size() != 0)
+      {
+         setEquipmentId(Integer.parseInt(twoDimensionalArray.get(0).get(0)));
+         // System.out.println(Integer.parseInt(twoDimensionalArray.get(0).get(0)));
+         setEquipmentName(twoDimensionalArray.get(0).get(1));
+         setEquipmentDescription(twoDimensionalArray.get(0).get(2));
+         setEquipmentCapacity(Integer.parseInt(twoDimensionalArray.get(0).get(3)));
+         // msd.close(connection);
+      }
+   
+      //msd.close(connection);
+
+   } // end fetch
+   
+   public void put() // put method - changes the data
+   {
+      Connection connection = msd.connect();
+      String query = "Update Equipment SET EquipID = " + getEquipmentId() + ", EquipmentName = '" + getEquipmentName() + "', EquipmentDescription = '" + getEquipmentDescription() + "', EquipmentCapacity = " + getEquipmentCapacity() + " Where EquipID = " + getEquipmentId() + ";";
+               
+      record = msd.setData(query);
+            
+      if(record > 0)
+      {
+         this.record = record;
+      }
+      
+      // msd.close(connection);
+   
+   } // end put   
+
+   public void post() // post method - inserts the data
+   { 
+      Connection connection = msd.connect();
+      String query = "INSERT INTO Equipment (EquipID, EquipmentName, EquipmentDescription, EquipmentCapacity)" + " VALUES (" + getEquipmentId() + ", '" +getEquipmentName()+"', '" + getEquipmentDescription() + "', " + getEquipmentCapacity() + " );";
+          
+      record = msd.setData(query);
+      
+     // msd.close(connection);
+   } // end post
+   
+   public void delete() // delete method - deletes the data
+   {
+      Connection connection = msd.connect();
+      String query = "DELETE FROM Equipment WHERE EquipID = " + getEquipmentId() + ";";
+      record = msd.setData(query);
+            
+      // msd.close(connection);
+   } // end delete
+   
+   /* Presentation Layer(?) */
+   public String toString()
+   {
+      String message =  "Equipment: " + getEquipmentId();
+      message += "\nEquipment Name: " + getEquipmentName();
+      message += "\nEquipment Description: " + getEquipmentDescription();
+      message += "\nEquipment Capacity: " + getEquipmentCapacity();
+      message += "/n/n" + getRecord() + " rows affected\n";
+      
+      record = 0;
+   
+      return message;
+   }
+   
+   public String numToString()
+   {
+      
+      String message =  "/n/n" + getRecord() + " rows affected\n";
+      
+      record = 0;
+      return message;
+      
+   }
+   
+
+}
---
+++ PE03/Riley_PE03/MySQLDatabase.java
@@ -0,0 +1,187 @@
+/* 
+   Name: Edward Riley
+   Professor: Stephen Zilora
+   Course: Database Connectivity and Access
+   Date: Feburary 14, 2020
+*/
+
+import java.sql.*;
+import javax.sql.*;
+import java.util.*;
+
+public class MySQLDatabase  
+{
+
+   // Connecting Classes Together
+   static main m = new main();
+   static Equipment equipment = new Equipment();
+
+   // Initialized Variables
+   //   static Connection connection = null;
+   static String hostMySQL = "jdbc:mysql://localhost/travel?";
+   static String driver = "com.mysql.jdbc.Driver";
+   static String user = "root";
+   static String password = "student";
+   static String databaseType = "MySQL";  
+   static private Connection connection = null; 
+   
+   
+   
+   
+   // Changed Connection to return connection instead of boolean
+   public static Connection connect()
+   {
+      try 
+      {
+         
+         // Loads driver
+         Class.forName(driver);     
+         
+         // Initiates Connection
+         // System.out.print("\nAttempting to initiate connection to " + url + " as " + user + "...");
+         Connection connection = DriverManager.getConnection(hostMySQL, user, password);
+         // System.out.print("Success!");
+      
+         //close(connection);
+         
+         // If all is fine and swell - return connection
+         return connection;
+      
+      
+      }
+      catch (SQLException e)
+      {
+         // System.out.println("Message: " + e.getMessage());
+         
+         // If all is NOT fine and swell - return null as inoperational
+         System.out.println(e);
+         return null;
+      }
+      catch (Exception e)
+      {
+         // System.out.println("Connection FAILED\nError: " + e.toString());
+      
+         // If all is NOT fine and swell - return null as inoperational
+         System.out.println(e);
+      
+         return null;
+      }
+   } // end connect method
+   
+   public static boolean close(Connection connection)
+   {
+      try 
+      {
+       //  System.out.print("\nAttempting to close connection...");
+       
+       // Receive ANY connection and attempt to close. 
+         connection.close();
+      
+       //  System.out.print("Success!");
+      }
+      catch (Exception e)
+      {
+       //  System.out.println("Error! Failed to close! Error Message: " + e.toString());
+         return false;
+      }
+      return true;
+   }
+
+   public String getURL()
+   {
+      return hostMySQL;
+   }
+   public String getDriverInfo()
+   {
+      return driver;
+   }
+   public String getPassword()
+   {
+      return password;
+   }
+   public String getUser()
+   {
+      return user;
+   }
+   public String getDatabaseType()
+   {
+      return databaseType;
+   }
+
+   public Connection getConnection()
+   {
+      return connection;
+   }   
+   public void setConnection(Connection _connection)
+   {
+      connection = _connection;
+   }
+   
+   
+   static MySQLDatabase msd = new MySQLDatabase();
+
+   public ArrayList<ArrayList<String>> getData(String query, int columnNum)
+   {
+      ArrayList<ArrayList<String>> twoDimensionalArray = new ArrayList<ArrayList<String>>();
+      ArrayList<String> info = new ArrayList<String>();  
+      
+      try
+      {
+      
+         // Prepare then execute statements. 
+         Statement stmt = connection.createStatement(); // The problem is... Connection is "null" 
+         ResultSet rs = stmt.executeQuery(query);
+         
+         int count = 0;
+         
+         while (rs.next())
+         {
+            twoDimensionalArray.add(info);  
+            for ( int i = 1; i <= columnNum; i++) 
+            {
+               twoDimensionalArray.get(count).add(rs.getString(i));
+            }
+                        
+            count++;  
+         }      
+      
+      }
+      catch (SQLException e)
+      {
+         System.out.println(e.toString());
+      }
+      
+      return twoDimensionalArray;
+   }
+   
+   
+   public int setData(String query)
+   {
+      try 
+      {
+         Statement stmt = connection.createStatement();
+         
+         // how many rows updated
+         int row = stmt.executeUpdate(query);
+         
+         return row;
+      }
+      catch (Exception e)
+      {
+         System.out.println(e.toString());
+         
+         // return error code
+         return -1;
+      }
+         
+   
+   
+   }
+   
+   
+   
+}
+
+   
+   
+
